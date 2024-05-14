520aca23276e86082a5510793a740de0e6d2857b
KlausDG <33721551+KlausDG@users.noreply.github.com>
2024-05-14 16:07:35 -0300

    feat: swagger (#6)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,33 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
+  @ApiOperation({ summary: 'Create user' })
+  @ApiResponse({
+    status: 201,
+    description: 'The user has been successfully created.',
+  })
+  @ApiResponse({ status: 401, description: 'Credentials Taken' })
   signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
   }
 
   @HttpCode(HttpStatus.OK)
   @Post('signin')
+  @ApiOperation({ summary: 'Login' })
+  @ApiResponse({
+    status: 201,
+    description: 'The user has been logged in.',
+  })
+  @ApiResponse({ status: 401, description: 'Credentials Incorrect' })
   signin(@Body() dto: AuthDto) {
     return this.authService.signin(dto);
   }
